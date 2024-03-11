bd4e9f8e084da159b3d68cf86e89f86b3ec14487
binh.phan@smsdome.com <vominhtrungpro@gmail.com>
2024-03-11 15:49:49 +0700

    update 03/11/2024

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import pymysql
 from flask import Flask, request, jsonify
+import jwt
+from jwt.exceptions import ExpiredSignatureError, InvalidTokenError
+import requests
+import datetime
+from flask_cors import CORS  # Import CORS module
+
+
+secret_key = "your_secret_key_here"
+
 
 app = Flask(__name__)
+CORS(app) 
 
 conn = pymysql.connect(
     host='localhost',
@@ -10,27 +20,123 @@ conn = pymysql.connect(
     database='vmt_music'
 )
 
-@app.route('/api/insert_music', methods=['POST'])
-def insert_music():
-    data = request.json
-    uuid = "uuid"
-    url = data.get('url')
-    status = 1
-    created_by = 0
-    updated_by = 0
+def get_google_public_keys():
+    response = requests.get("https://www.googleapis.com/oauth2/v3/certs")
+    return response.json()
+
+
+def validate_google_jwt(token):
+    public_keys = get_google_public_keys()
+    for key in public_keys.values():
+        try:
+            payload = jwt.decode(token, key, algorithms=['RS256'])
+            return True
+        except ExpiredSignatureError:
+            return False
+        except InvalidTokenError:
+            pass  
+    return True
+
+
+# @app.before_request
+# def authenticate():
+#     token = request.headers.get('Authorization')
+
+#     if not token or not validate_google_jwt(token):
+#         return jsonify({'error': 'Unauthorized access.'}), 401
+
+def get_email():
+    token = request.headers.get('Authorization')
+    if token != '':
+        payload = jwt.decode(token, options={"verify_signature": False})
+        email = payload.get("email")
+        return email
+    else:
+        return ''
     
-    if None in (uuid, url, created_by, updated_by):
-        return jsonify({'error': 'Thiếu thông tin.'}), 400
+def create_user_google(email):
+    uuid = "uuid"
+    email = email
+    status = 1
+    login_type = "google"
+
+    try:
+        with conn.cursor() as cursor:
+            sql_query = """INSERT INTO users (uuid, email, status, login_type) 
+                           VALUES (%s, %s, %s, %s)"""
+            cursor.execute(sql_query, (uuid, email, status, login_type))
+            conn.commit()
+
+            user_id = cursor.lastrowid
+            
+            return user_id,''
+            
+    except Exception as e:
+        return None,str(e)
+        
+def check_email_exist(email):
+    try:
+        with conn.cursor() as cursor:
+            cursor.execute("SELECT * FROM users WHERE email = %s LIMIT 1", (email,))
+            user = cursor.fetchone()
+            if user:
+                current_time = datetime.datetime.now()
+                lr = user[6]  
+                time_difference = current_time - lr
+                if time_difference.total_seconds() <= 5 * 60: 
+                    return None,'Request limited'
+                else:
+                    return user[0],''
+            else:
+                return None,''
+
+    except Exception as e:
+        return None,str(e)
+    
+def create_music_queue(user_id,url):
+    user_id = user_id
+    url = url
+    if None in (user_id, url):
+        return 'Thiếu thông tin'
     
     try:
         with conn.cursor() as cursor:
-            sql_query = """INSERT INTO music_queue (uuid, url, status, created_by, updated_by) 
-                           VALUES (%s, %s, %s, %s, %s)"""
-            cursor.execute(sql_query, (uuid, url, status, created_by, updated_by))
-            conn.commit()
-        return jsonify({'message': 'Dữ liệu đã được chèn thành công.'}), 200
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
+            sql_query1 = """INSERT INTO music_queue (uuid, url, status, requested_by, created_by, updated_by) 
+                           VALUES (%s, %s, %s, %s, %s, %s)"""
+            cursor.execute(sql_query1, ("uuid", url, 1, user_id, 0, 0))
 
+            sql_query2 = """UPDATE users SET last_request = CURRENT_TIMESTAMP 
+                           WHERE id = %s"""
+            cursor.execute(sql_query2, (user_id,))
+
+            conn.commit()
+
+            return ''
+    except Exception as e:
+        return str(e)
+    
+@app.route('/api/insert_music', methods=['POST'])
+def insert_music():
+    email = get_email()
+    if email == '':
+        return jsonify({'error': 'Unauthorized access.'}), 401
+    else:
+        user_id,err = check_email_exist(email)
+        if err != '':
+            return jsonify({'error': err}), 500
+        else:
+            if user_id == None:
+                user_id,err = create_user_google(email)
+                if err != '':
+                    return jsonify({'error': err}), 500
+
+    data = request.json
+    url = data.get('url')
+    result = create_music_queue(user_id,url)
+    if result != '':
+        return jsonify({'error': result}), 500
+    else:
+        return jsonify({'message': 'Dữ liệu đã được chèn thành công.'}), 200
+    
 if __name__ == '__main__':
     app.run(debug=True)
